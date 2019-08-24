f22a76a9f98e916dd2a86149554de2bf14ba0330
Ariana <ariana.villegas@utec.edu.pe>
2019-08-24 11:23:04 -0500

    Resuelto

--- main.cpp
+++ main.cpp
@@ -2,7 +2,28 @@
 
 using namespace std;
 
+void jumps(unsigned int numwalls){
+    unsigned int upper=0;
+    unsigned int lower=0;
+    unsigned int start, next; cin >> start;
+    for (unsigned int i=1; i<numwalls; i++){
+        cin >> next;
+        if (start < next) upper++;
+        else if (start > next) lower++;
+        start = next;
+    }
+    printf("%u %u\n",upper,lower);
+}
+
 int main() {
-	return EXIT_SUCCESS;
+    unsigned int numcases, numwalls;
+    cin >> numcases; numcases++;
+    for (int i=1; i<numcases; i++){
+        cin >> numwalls;
+        printf("Case %u: ",i);
+        jumps(numwalls);
+    }
+
+    return EXIT_SUCCESS;
 }
 
